8bb8dd409e3856877895e825c033a83d26ad5e4a
vitorsei <seiji.vitor@gmail.com>
2016-09-10 11:45:06 -0300

    Adding new course

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -14,6 +14,7 @@ class ManageCoursePage extends React.Component {
         };
 
         this.updateCourseState = this.updateCourseState.bind(this);
+        this.saveCourse = this.saveCourse.bind(this);
     }
 
     updateCourseState(event) {
@@ -23,11 +24,18 @@ class ManageCoursePage extends React.Component {
         return this.setState({course: course});
     }
 
+    saveCourse(event) {
+        event.preventDefault();
+        this.props.actions.saveCourse(this.state.course);
+        this.context.router.push('/courses');
+    }
+
     render() {
         return (
             <CourseForm
                 allAuthors={this.props.authors}
                 onChange={this.updateCourseState}
+                onSave={this.saveCourse}
                 course={this.state.course}
                 errors={this.state.errors}
             />
@@ -37,7 +45,13 @@ class ManageCoursePage extends React.Component {
 
 ManageCoursePage.propTypes = {
     course: PropTypes.object.isRequired,
-    authors: PropTypes.array.isRequired
+    authors: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
+};
+
+//Pull in the React Router context so router is available on this.context.router.
+ManageCoursePage.contextTypes = {
+    router: PropTypes.object
 };
 
 function mapStateToProps(state, ownProps) {
