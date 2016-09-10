2e6388176bf5d052fb827e9e6a801f931c2c6b45
vitorsei <seiji.vitor@gmail.com>
2016-09-10 17:29:13 -0300

    Hide Preloader Based on Status

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,8 +1,9 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
+import {beginAjaxCall} from './ajaxStatusActions';
 
 export function loadCoursesSuccess(courses) {
-    return {type: types.LOAD_COURSES_SUCCESS, courses}
+    return {type: types.LOAD_COURSES_SUCCESS, courses};
 }
 
 export function createCourseSuccess(course) {
@@ -15,21 +16,23 @@ export function updateCourseSuccess(course) {
 
 export function loadCourses() {
     return function (dispatch) {
+        dispatch(beginAjaxCall());
         return courseApi.getAllCourses().then(courses => {
             dispatch(loadCoursesSuccess(courses));
         }).catch(error => {
             throw(error);
         });
-    }
+    };
 }
 
 export function saveCourse(course) {
     return function (dispatch, getState) {
+        dispatch(beginAjaxCall());
         return courseApi.saveCourse(course).then(savedCourse => {
             course.id ? dispatch(updateCourseSuccess(savedCourse)) :
                 dispatch(createCourseSuccess(savedCourse));
         }).catch(error => {
             throw(error);
         });
-    }
+    };
 }
