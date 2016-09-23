3a16bcd2cd5328de49c9d5535c03ec5d6a1595b4
vitorsei <seiji.vitor@gmail.com>
2016-09-23 14:40:20 -0300

    Sort courses alphabetically

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -2,21 +2,36 @@ import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
 export default function courseReducer(state = initialState.courses, action) {
+    function sortCourseByTitle(courses) {
+        courses.sort((a, b) => a.title.toLowerCase() > b.title.toLowerCase());
+    }
+
     switch (action.type) {
         case types.LOAD_COURSES_SUCCESS:
+        {
+            sortCourseByTitle(action.courses);
             return action.courses;
+        }
 
-        case types.CREATE_COURSE_SUCCESS:
-            return [
+        case types.CREATE_COURSE_SUCCESS: {
+            let courses = [
                 ...state,
                 Object.assign({}, action.course)
             ];
 
-        case types.UPDATE_COURSE_SUCCESS:
-            return [
+            sortCourseByTitle(courses);
+            return courses;
+        }
+
+        case types.UPDATE_COURSE_SUCCESS: {
+            let courses = [
                 ...state.filter(course => course.id !== action.course.id),
                 Object.assign({}, action.course)
             ];
+
+            sortCourseByTitle(courses);
+            return courses;
+        }
 
         case types.DELETE_COURSE_SUCCESS: {
             let index = state.findIndex(course => course.id !== action.courseId);
