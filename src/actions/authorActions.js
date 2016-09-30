d04622b8922942c3037e866b34792be1371de0a1
vitorsei <seiji.vitor@gmail.com>
2016-09-30 06:00:52 -0300

    Saving author action

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -1,9 +1,17 @@
 import AuthorApi from '../api/mockAuthorApi';
 import * as types from './actionTypes';
-import {beginAjaxCall} from './ajaxStatusActions';
+import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
 
 export function loadAuthorsSuccess(authors) {
     return {type: types.LOAD_AUTHORS_SUCCESS, authors};
+}
+
+export function createAuthorSuccess(author) {
+    return {type: types.CREATE_AUTHOR_SUCCESS, author};
+}
+
+export function updateAuthorSuccess(author) {
+    return {type: types.UPDATE_AUTHOR_SUCCESS, author};
 }
 
 export function loadAuthors() {
@@ -12,6 +20,19 @@ export function loadAuthors() {
         return AuthorApi.getAllAuthors().then(authors => {
             dispatch(loadAuthorsSuccess(authors));
         }).catch(error => {
+            throw(error);
+        });
+    };
+}
+
+export function saveAuthor(author) {
+    return function (dispatch, getState) {
+        dispatch(beginAjaxCall());
+        return AuthorApi.saveAuthor(author).then(savedAuthor => {
+            author.id ? dispatch(updateAuthorSuccess(savedAuthor)) :
+                dispatch(createAuthorSuccess(savedAuthor));
+        }).catch(error => {
+            dispatch(ajaxCallError(error));
             throw(error);
         });
     };
