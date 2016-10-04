e13f19e474c1473e3809194a3fc9e2fb83445478
vitorsei <seiji.vitor@gmail.com>
2016-10-04 07:48:49 -0300

    Deleting author

--- src/components/course/ManageCoursePage.test.js
+++ src/components/course/ManageCoursePage.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import expect from 'expect';
-import {mount, shallow} from 'enzyme';
+import {mount} from 'enzyme';
 import {ManageCoursePage} from './ManageCoursePage';
 
 describe('Manage Course Page', () => {
@@ -14,12 +14,11 @@ describe('Manage Course Page', () => {
             },
             course: {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''}
         };
-
-        const wrapper = mount(<ManageCoursePage {...props}/>);
+        const context = { router: { setRouteLeaveHook: (a, b) => '' } };
+        const wrapper = mount(<ManageCoursePage {...props} />, {context});
         const saveButton = wrapper.find('input').last();
         expect(saveButton.prop('type')).toBe('submit');
         saveButton.simulate('click');
         expect(wrapper.state().errors.title).toBe('Title must be at least 5 characters.');
-
     });
 });
