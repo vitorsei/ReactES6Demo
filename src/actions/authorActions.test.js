f0656b0b42967db852b6cabffe2a318726406e90
vitorsei <seiji.vitor@gmail.com>
2016-10-07 19:58:29 -0300

    Author Actions test

---
+++ src/actions/authorActions.test.js
@@ -0,0 +1,56 @@
+import expect from 'expect';
+import * as authorActions from './authorActions';
+import * as types from './actionTypes';
+import thunk from 'redux-thunk';
+import nock from 'nock';
+import configureMockStore from 'redux-mock-store';
+
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+
+describe('Async Actions', () => {
+    afterEach(() => {
+        nock.cleanAll();
+    });
+
+    it('should create BEGIN_AJAX_CALL and LOAD_AUTHORS_SUCCESS when loading authors', (done) => {
+        // Here's an example call to nock.
+        // nock('http://example.com/')
+        //   .get('/authors')
+        //   .reply(200, { body: { author: [{ id: 1, firstName: 'Cory', lastName: 'House'}] }});
+
+        const expectedActions = [
+            {type: types.BEGIN_AJAX_CALL},
+            {type: types.LOAD_AUTHORS_SUCCESS, body: {authors: [{id: 'clean-code', title: 'Clean Code'}]}}
+        ];
+
+        const store = mockStore({authors: []}, expectedActions);
+        store.dispatch(authorActions.loadAuthors()).then(() => {
+            const actions = store.getActions();
+            expect(actions[0].type).toEqual(types.BEGIN_AJAX_CALL);
+            expect(actions[1].type).toEqual(types.LOAD_AUTHORS_SUCCESS);
+            done();
+        });
+    });
+
+    it('should not allow delete authors associated with courses', (done) => {
+        // Here's an example call to nock.
+        // nock('http://example.com/')
+        //   .get('/authors')
+        //   .reply(200, { body: { author: [{ id: 1, firstName: 'Cory', lastName: 'House'}] }});
+
+        const expectedActions = [
+            {type: types.BEGIN_AJAX_CALL},
+            {type: types.LOAD_AUTHORS_SUCCESS, body: {authors: [{firstName: 'Vitor', lastName: 'Iwamura'}]}}
+        ];
+
+        const store = mockStore({courses: [{id: '1'}]}, expectedActions);
+        store.dispatch(authorActions.deleteAuthor('1')).then(() => {
+            const actions = store.getActions();
+            expect(actions[0].type).toEqual(types.BEGIN_AJAX_CALL);
+            expect(actions[1].type).toEqual(types.DELETE_AUTHOR_SUCCESS);
+            done();
+        });
+
+    });
+});
